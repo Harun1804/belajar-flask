b9c9610bca0c70562c6db9c6ce7563cc998d675d
Harun1804 <Harun.arrasyid1804@gmail.com>
2023-12-18 17:45:08 +0700

    Connecting To Mysql

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,12 @@
 from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app) # database instance
+migrate = Migrate(app, db) # migration engine instance
 
+from app.model import user
 from app import routes
