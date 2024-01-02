80067a40d2a7015d94c67f3110f2925b36b1512b
Harun1804 <Harun.arrasyid1804@gmail.com>
2024-01-02 15:17:40 +0700

    Add Swagger

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -10,6 +12,18 @@ db = SQLAlchemy(app) # database instance
 migrate = Migrate(app, db) # migration engine instance
 
 jwt = JWTManager(app)
+
+SWAGGER_URL = '/swagger'
+API_URL = '/static/swagger.json'
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,
+    API_URL,
+    config={
+        'app_name': "Belajar Flask"
+    }
+)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+CORS(app)
 
 from app.model import user, dosen, mahasiswa, gambar
 from app import routes
